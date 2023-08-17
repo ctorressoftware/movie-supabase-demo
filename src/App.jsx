c574eb07862deb2201df1324ff35d06f49b28d79
Carlos Eduardo Torres Graciano <cetorres@humano.com.do>
2023-08-17 16:57:04 -0400

    [ADD] Uploading image files to Supabase Storage and programmig form to store movie data

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { supabase } from './lib/api';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [refreshMovies, setRefreshMovies] = useState(false);
   const [form, setForm] = useState({});
 
   useEffect(() => {
@@ -19,16 +20,31 @@ function App() {
 
     getMovies();
 
-  }, [])
+  }, [refreshMovies])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const response = await uploadFile(form.logo);
+
+    console.log(response);
+
     const { data, error } = await supabase
       .from('Movie')
       .insert([
-        {name: form.nombrePelicula, description: form.descripcion, logoLink: form.linkLogo},
+        {
+          name: form.nombrePelicula, 
+          description: form.descripcion, 
+          logoLink: `https://kscptdezabdcaceixcwg.supabase.co/storage/v1/object/public/movie-covers/${response.path}`},
       ])
-      .select()
+      .select();
+
+      if (error){
+        alert("Ha ocurrido un error.");
+      } else {
+        setRefreshMovies(true);
+        alert("Pelicula insertada correctamente.");
+      }
   }
 
   const handleChange = (e) => {
@@ -37,6 +53,31 @@ function App() {
       [e.target.name]: e.target.value,
     })
   };
+
+  const handleFileChange = (e) => {
+    setForm({
+      ...form,
+      ["logo"]: e.target.files[0],
+    })
+  };
+
+   const uploadFile = async (file) => {
+    console.log(file);
+    const { data, error } = await supabase.storage.from('movie-covers').upload(`image_${generateRandomNumber()}.jpg`, file)
+    if (error) {
+      console.error(error);
+      return data;
+    } else {
+      return data;
+    }
+  }
+
+  function generateRandomNumber() {
+    const randomNumber = Math.random() * 1000000000000; // Multiplica por un número grande
+    const roundedNumber = Math.floor(randomNumber); // Redondea hacia abajo
+  
+    return roundedNumber;
+  }
 
   return (
     <>
@@ -48,7 +89,7 @@ function App() {
         <input id='nombrePelicula' name='nombrePelicula' onChange={handleChange} placeholder='Nombre de la pelicula' />
 
         <label htmlFor='linkLogo'>Foto de la pelicula</label>
-        <input id='linkLogo' name='linkLogo' onChange={handleChange} placeholder='Foto de la pelicula' />
+        <input type='file' id='linkLogo' name='linkLogo' onChange={handleFileChange} placeholder='Foto de la pelicula' />
 
         <label htmlFor='descripcion'>Descripcion</label>
         <input id='descripcion' name='descripcion' onChange={handleChange} placeholder='Descripcion de la pelicula' />
